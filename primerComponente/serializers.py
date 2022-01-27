b9cde946e629d77fc9720c7e4e0927f6e5ea25ca
203433 <203433@ids.upchiapas.edu.mx>
2022-01-27 04:13:05 -0600

    Adition of views and class service

--- primerComponente/serializers.py
+++ primerComponente/serializers.py
@@ -1,10 +1,9 @@
 from dataclasses import fields
 from rest_framework import routers, serializers, viewsets
-
-# importacion de modelos
+#Importancion de modelos
 from primerComponente.models import PrimerTabla
-
 class PrimerTablaSerializer(serializers.ModelSerializer):
     class Meta:
         model = PrimerTabla
-        fields= ('nombre','edad')
+        fields = ('nombre','edad') 
+        fields = ('__all__') 
